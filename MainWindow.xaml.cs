7b3112b29ec6e2bf3f11b82a6caa34b73cc391f2
Nolan Flammini <nolantheturtle@gmail.com>
2020-03-28 11:23:04 -0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using NAudio;
+using NAudio.Wave;
+using NAudio.CoreAudioApi;
+using NAudio.MediaFoundation;
+
+namespace AudioWave
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static MainWindow Instance;
+        internal Wave wave;
+        internal SideWindow side;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Instance = this;
+            wave = new Wave();
+            side = new SideWindow();
+            side.Show();
+        }
+
+        private void On_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            side.Close();
+            wave.audioOut.Dispose();
+            wave.reader.Dispose();
+        }
+    }
+    public class Wave
+    {
+        internal AudioFileReader reader;
+        private float[] data;
+        private MainWindow Window;
+        internal WasapiOut audioOut;
+        public Wave()
+        {
+            Window = MainWindow.Instance;
+            Display();
+        }
+        public void Init(string file)
+        {
+            reader = new AudioFileReader(file);
+            data = Buffer(0);
+            audioOut.Dispose();
+            audioOut = new WasapiOut();
+            Window.wave.audioOut.PlaybackStopped += MainWindow.Instance.side.On_PlaybackStopped;
+            audioOut.Init(reader);
+            audioOut.Play();
+        }
+
+        Action method;
+        private void Display()
+        {
+            method = delegate ()
+            {
+                Thread.Sleep(1000 / 150);
+                if (reader != null && audioOut.PlaybackState == PlaybackState.Playing)
+                    GenerateImage();
+                MainWindow.Instance.graph.Dispatcher.BeginInvoke(method, System.Windows.Threading.DispatcherPriority.Background);
+            };
+            MainWindow.Instance.graph.Dispatcher.BeginInvoke(method, System.Windows.Threading.DispatcherPriority.Background);
+        }
+        private void GenerateImage()
+        {
+            int width = (int)Window.graph.Width;
+            int height = (int)Window.graph.Height;
+            int stride = width * ((PixelFormats.Bgr24.BitsPerPixel + 7) / 8);
+            using (Bitmap bmp = new Bitmap(width, height, stride, System.Drawing.Imaging.PixelFormat.Format24bppRgb, new IntPtr()))
+            {
+                using (Graphics graphic = Graphics.FromImage(bmp))
+                {
+                    PointF[] points = new PointF[width];
+                    data = Buffer(points.Length);
+                    for (int i = 0; i < points.Length; i++)
+                    {
+                        points[i] = new PointF(i, height / 2 * data[i] + height / 2);
+                    }
+                    graphic.FillRectangle(System.Drawing.Brushes.Black, new System.Drawing.Rectangle(0, 0, width, height));
+                    graphic.DrawCurve(System.Drawing.Pens.White, points);
+                }
+                var bmpData = bmp.LockBits(new System.Drawing.Rectangle(0, 0, width, height), System.Drawing.Imaging.ImageLockMode.ReadOnly, bmp.PixelFormat);
+                
+                Window.graph.Source = BitmapSource.Create(width, height, 96f, 96f, PixelFormats.Bgr24, null, bmpData.Scan0, stride * height, stride);
+                bmp.UnlockBits(bmpData);
+            }
+        }
+        private float[] Buffer(int length)
+        {
+            long position = reader.Position;
+            float[] buffer = new float[length];
+            reader.ToSampleProvider().Read(buffer, 0, buffer.Length);
+            reader.Position = position;
+            return buffer;
+        }
+    }
+}
---
+++ SideWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using NAudio.Wave;
+
+namespace AudioWave
+{
+    /// <summary>
+    /// Interaction logic for SideWindow.xaml
+    /// </summary>
+    public partial class SideWindow : Window
+    {
+        private MainWindow Window;
+        private int current;
+        private bool playing;
+        private bool looping;
+        public SideWindow()
+        {
+            InitializeComponent();
+            Window = MainWindow.Instance;
+            Window.wave.audioOut = new WasapiOut();
+        }
+        public void On_PlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            if (looping)
+            {
+                Window.wave.reader.Seek(0, System.IO.SeekOrigin.Begin);
+                Window.wave.audioOut.Play();
+                return;
+            }
+            if (playing)
+            {
+                current++;
+                if (current < Playlist.Count)
+                {
+                    Window.wave.Init(Playlist[current]);
+                }
+            }
+        }
+        private void On_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Label label = (Label)e.Source;
+            label.Foreground = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        }
+
+        private void On_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Label label = (Label)e.Source;
+            label.Foreground = new SolidColorBrush(Color.FromRgb(150, 150, 150));
+        }
+
+        public List<string> Playlist = new List<string>();
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.OpenFileDialog();
+            dialog.Multiselect = true;
+            dialog.Title = "Select Audio Files";
+            dialog.InitialDirectory = Environment.GetEnvironmentVariable("USERPROFILE");
+            dialog.Filter = "Audio files|*.mp3;*.wav|WAV files (*.wav)|*.wav|MP3 files (*.mp3)|*.mp3";
+            dialog.ShowDialog();
+            string[] files = dialog.SafeFileNames;
+            if (files.Length > 0)
+            {
+                for (int i = 0; i < files.Length; i++)
+                {
+                    Playlist.Add(dialog.FileNames[i]);
+                    var item = new ListBoxItem();
+                    item.Content = files[i].Substring(0, files[i].Length - 4);
+                    playlist.Items.Add(item);
+                }
+            }
+        }
+
+        private void On_MouseClear(object sender, MouseButtonEventArgs e)
+        {
+            if (playlist.SelectedIndex != -1)
+            {
+                Playlist.RemoveAt(playlist.SelectedIndex);
+                playlist.Items.RemoveAt(playlist.SelectedIndex);
+            }
+        }
+
+        private void On_Play(object sender, MouseButtonEventArgs e)
+        {
+            playing = true;
+            current = playlist.SelectedIndex;
+            if (playlist.SelectedIndex != -1)
+            {
+                Window.wave.Init(Playlist[playlist.SelectedIndex]);
+                playlist.SelectedIndex = -1;
+            }
+            else
+            {
+                Window.wave.audioOut.Play();
+            }
+        }
+
+        private void On_Stop(object sender, MouseButtonEventArgs e)
+        {
+            playing = false;
+            Window.wave.audioOut.Stop();
+            Window.wave.reader.Seek(0, System.IO.SeekOrigin.Begin);
+        }
+
+        private void On_Pause(object sender, MouseButtonEventArgs e)
+        {
+            if (Window.wave.audioOut.PlaybackState == PlaybackState.Playing)
+            {
+                Window.wave.audioOut.Pause();
+            }
+        }
+
+        private void On_Loop(object sender, MouseButtonEventArgs e)
+        {
+            looping = !looping;
+            label_loop.Foreground = looping ? new SolidColorBrush(Color.FromRgb(0, 0, 0)) : new SolidColorBrush(Color.FromRgb(150, 150, 150));
+        }
+
+        private void On_Clear(object sender, MouseButtonEventArgs e)
+        {
+            playlist.Items.Clear();
+            Playlist.Clear();
+        }
+    }
+}
